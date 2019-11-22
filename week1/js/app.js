2a1d27427123e10452625f24c5d3f00811f5ece8
Abdelrahman <alalqi10100@gmail.com>
2019-11-22 09:08:46 +0100

    syntax

---
+++ week1/js/app.js
@@ -0,0 +1,170 @@
+// let age = 23;
+// let year = 2019;
+
+// console.log(age , year);
+
+// const points = 100;
+
+// console.log(points);
+
+// var score = 75;
+// console.log(score);
+
+// console.log("hello world")
+
+// let email = "mario@gmail.com"
+
+
+// console.log(email);
+
+// let firstName = "abdu"
+// let lastName = "alallaqi"
+
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// console.log(fullName.length);
+
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result);
+
+// let index = email.indexOf("@");
+// console.log(index);
+
+// let email = "mario@gmail.com"
+
+// let result = email.lastIndexOf("l");
+// console.log(result);
+
+// let result = email.slice(0,10);
+
+// let result = email.substr(4,10)
+
+// let result = email.replace("m","w");
+// let result = email.()
+// console.log(result);
+
+// let radius = 10;
+// const pi = 3.14;
+
+// console.log(radius, pi);
+
+
+// mathopperaters
+
+// console.log(10 / 5);
+
+// let result = radius % 3;
+
+// let result = pi * radius**2;
+
+// let result = 5 * (10-3)**2;
+
+// let likes = 10;
+// likes++;
+// likes--;
+// likes += 10;
+
+// let result = " the blog has " + likes + " likes";
+
+// console.log(result);
+
+
+
+
+
+// console.log(likes);
+
+// const titel = "best reads of 2019";
+// const author = "mario";
+// const likes = 30;
+
+// let result = "The blog called " + titel + " by " + author + " has " + likes + " likes.";
+// console.log(result);
+// template string
+// let results = `the blog called ${titel} by ${author} has ${likes} likes`;
+// console.log(result);
+
+// let html = `<h2>${titel}</h2>
+// <p>${author}</p>
+// <h2>${likes}</h2>`
+
+// let ninjas = ["shaun", "ryu", "chunli"];
+
+// ninjas[1] = "fathi";
+
+// console.log(ninjas[1]);
+
+// let ages = [20,25,30,30];
+
+// let random = ["shaun", "crystal", 30, 20];
+// console.log(ages, random);
+// console.log(ninjas.length);
+
+// let result = ninjas.join(",");
+
+
+// let result = ninjas.indexOf("chunli");
+// let results = ninjas.concat(["kenny", "lol"]);
+// console.log(results);
+
+// let result = ninjas.push("ken");
+// result = ninjas.pop();
+// console.log(ninjas);
+
+// let age = null;
+
+// console.log(age, age + 3, `the age is ${age}`);
+
+// console.log(true, false, "true");
+
+// let email = "luigi@the netninja.co.uk";
+// let names = ["marin", "luigi", "tod"]
+
+// let result = email.includes("@");
+// let result = names.includes("bowser");
+// let age = 25;
+
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 35);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 20);
+// console.log(result);
+
+// let name = "shaun";
+
+// console.log(name == "shaun");
+// console.log(name > "crystal");
+// console.log(name > "Shaun");
+
+// let age = 25;
+
+// console.log(age == 25);
+// console.log(age == "25");
+
+// console.log(age != 25);
+// console.log(age != "25");
+
+// console.log(age === 25);
+// console.log(age === "25");
+
+// console.log(age !== 25);
+// console.log(age !== "25");
+
+let score = "100";
+// score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let results = Number("hello");
+// let results = String(50);
+// console.log(results);
+
+let results = Boolean("");
+console.log(results, typeof results); 
+
